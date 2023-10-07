027eb5de7d9637ff202b3c00c1fc66e626307461
Kunal Mahadule <110099674+kunalmahadule@users.noreply.github.com>
2023-10-07 13:33:16 +0530

    GFG problem to find last Occurrence of one

---
+++ Last_index_of_one.py
@@ -0,0 +1,20 @@
+# S = '00001'
+S = '10'
+print(S.rfind('1'))
+
+# print(S.index(S[len(S)-1]))
+# print(S.index('1'))
+# for i in range(len(S)):
+#     if
+#     print(S.index('1',-1,))
+
+
+# out = 4
+# print(S[4])
+# arr = []
+# for i in range(len(S)):
+#     arr.append(S[i])
+#
+# n = len(arr)
+# print(arr)
+# print(arr.index(arr[n-1]))
